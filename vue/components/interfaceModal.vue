6d116d1ac947e726fb9230d38f5fbe072e5a237b
Daugvinas <daugvinasr@gmail.com>
2022-08-17 12:36:36 +0300

    v3

--- vue/components/interfaceModal.vue
+++ vue/components/interfaceModal.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <a-modal :title="'Interface: ' + interfaceName" v-model="$parent.modalVisibility" :width="500" style="">
-      <vuci-form uci-config="network_interfaces" @applied="$parent.handleSubmit()">
+      <vuci-form uci-config="network_interfaces" @applied="$parent.handleSubmit(sid, interfaceName)">
         <vuci-named-section type="interface" :name="sid" :label="'Network interface'" :collapsible="false"
           v-slot="{ s }" :card="false">
           <vuci-form-item-select :uci-section="s" :label="'Protocol'" name="protocol" initial="None"
-            :options="protocols" @change="showAdditionalInformation" />
-          <div v-if="$parent.showAdditionalFields">
-            <vuci-form-item-input :uci-section="s" :label="'Address'" name="address" rules="ip4addr" value="1.1.1.1" />
-            <vuci-form-item-input :uci-section="s" :label="'Netmask'" name="netmask" rules="netmask4" />
-            <vuci-form-item-input :uci-section="s" :label="'Gateway'" name="gateway" rules="ip4addr" />
+            :options="protocols" @change="showAdditionalInformation" :required="true" />
+          <div v-if="$parent.selectedProtocol === 'static'">
+            <vuci-form-item-input :uci-section="s" :label="'Address'" name="address" rules="ip4addr" :required="true"/>
+            <vuci-form-item-input :uci-section="s" :label="'Netmask'" name="netmask" rules="netmask4" :required="true" />
+            <vuci-form-item-input :uci-section="s" :label="'Gateway'" name="gateway" rules="ip4addr" :required="true" />
             <vuci-form-item-list :uci-section="s" :label="'DNS'" name="dns" rules="ip4addr" />
           </div>
         </vuci-named-section>
@@ -34,11 +34,7 @@ export default {
   },
   methods: {
     showAdditionalInformation (self) {
-      if (self.model === 'static') {
-        this.$parent.showAdditionalFields = true
-      } else {
-        this.$parent.showAdditionalFields = false
-      }
+      this.$parent.selectedProtocol = self.model
     }
   }
 }
